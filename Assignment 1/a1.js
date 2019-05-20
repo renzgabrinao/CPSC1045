6f5f4de092a3393c7e599e1344bb0dcb00af11ba
renzgabrinao <rgabrinao1@gmail.com>
2019-05-19 21:32:58 -0700

    Number 2 Finish

--- Assignment 1/a1.js
+++ Assignment 1/a1.js
@@ -3,10 +3,14 @@ function nosesAndFeet() {
 	//use the outputParagraph variable to access the div element
 	let outputParagraph = document.getElementById("output");
 
-	//Ask the user to enter the number of people and number of pets
+	//Ask the user to enter the number of people and number of pets	
+	const people = Number(prompt('How many people live at your home?'));
+	const pets = Number(prompt("How many pets live at your home?"));
 
 	//Calculate the number of noses/feet
-
+	const noses = people + pets;
+	const feet = (4 * pets) + (2 * people); 
 	//Display the output	
-	outputParagraph.innerHTML = "The appropriate output should go here.";
+	//outputParagraph.innerHTML = "The people and pets at your home have a total of " + noses + " noses and " + feet + " feet.";
+	outputParagraph.innerHTML = `The people and pets at your home have a total of ${noses} noses and ${feet} feet.`;
 }
