48c668ba462016dd3f259e460b7068d02a0968b0
renzgabrinao <rgabrinao1@gmail.com>
2019-05-19 21:21:20 -0700

    a1

---
+++ Assignment 1/a1.js
@@ -0,0 +1,12 @@
+	
+function nosesAndFeet() {	
+	//use the outputParagraph variable to access the div element
+	let outputParagraph = document.getElementById("output");
+
+	//Ask the user to enter the number of people and number of pets
+
+	//Calculate the number of noses/feet
+
+	//Display the output	
+	outputParagraph.innerHTML = "The appropriate output should go here.";
+}
