ab564613b66f8bb1dce5d755903cd0cd4e8516a0
renzgabrinao <rgabrinao1@gmail.com>
2019-05-23 21:59:53 -0700

    change to let

--- Assignment 2/a2.js
+++ Assignment 2/a2.js
@@ -3,33 +3,33 @@
 let expenses = 0;
 let income = 0;
 
-const formatDollarAmount = (money) => `$${money.toFixed(2)}`;
+let formatDollarAmount = (money) => `$${money.toFixed(2)}`;
 
-const addListItem = (id, text) => {
-    const node = document.createElement("li");
-    const newText = document.createTextNode(text);
+let addListItem = (id, text) => {
+    let node = document.createElement("li");
+    let newText = document.createTextNode(text);
     node.appendChild(newText);
 
     document.getElementById(id).innerHTML += node.innerHTML + "<br />";
 }
 
-const updateTotals = () => {
+let updateTotals = () => {
     document.getElementById("expTotal").innerHTML = formatDollarAmount(expenses);
     document.getElementById("incTotal").innerHTML = formatDollarAmount(income);
     document.getElementById("total").innerHTML = formatDollarAmount(12 * (income - expenses));
 }
 
-const addIncome = () => {
-    const item = prompt("What item to add?");
-    const money = +prompt("How much?");
+let addIncome = () => {
+    let item = prompt("What item to add?");
+    let money = +prompt("How much?");
     addListItem("income", `${formatDollarAmount(money)}: ${item}`)
     income += money;
     updateTotals();
 }
 
-const addExpense = () => {
-    const item = prompt("What item to add?");
-    const money = +prompt("How much?");
+let addExpense = () => {
+    let item = prompt("What item to add?");
+    let money = +prompt("How much?");
     addListItem("expenses", `${formatDollarAmount(money)}: ${item}`)
     expenses += money;
     updateTotals();
