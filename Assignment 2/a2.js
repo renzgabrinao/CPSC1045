e803e13047d0aff7df36660f1f7898dff41f97c3
renzgabrinao <rgabrinao1@gmail.com>
2019-05-23 21:55:29 -0700

    finish assign 2

--- Assignment 2/a2.js
+++ Assignment 2/a2.js
@@ -1,17 +1,36 @@
 "use strict";
 
-let x = 0;
-let y = 0;
+let expenses = 0;
+let income = 0;
 
-const formatDollarAmount = (money) => {
-    console.log(`$${money.toFixed(2)}`);
-    
-}
+const formatDollarAmount = (money) => `$${money.toFixed(2)}`;
 
 const addListItem = (id, text) => {
     const node = document.createElement("li");
     const newText = document.createTextNode(text);
     node.appendChild(newText);
 
-    document.getElementById(id).innerHTML += node.innerHTML;
+    document.getElementById(id).innerHTML += node.innerHTML + "<br />";
+}
+
+const updateTotals = () => {
+    document.getElementById("expTotal").innerHTML = formatDollarAmount(expenses);
+    document.getElementById("incTotal").innerHTML = formatDollarAmount(income);
+    document.getElementById("total").innerHTML = formatDollarAmount(12 * (income - expenses));
+}
+
+const addIncome = () => {
+    const item = prompt("What item to add?");
+    const money = +prompt("How much?");
+    addListItem("income", `${formatDollarAmount(money)}: ${item}`)
+    income += money;
+    updateTotals();
+}
+
+const addExpense = () => {
+    const item = prompt("What item to add?");
+    const money = +prompt("How much?");
+    addListItem("expenses", `${formatDollarAmount(money)}: ${item}`)
+    expenses += money;
+    updateTotals();
 }
