58d32a86da706d411b266cc5daf1a8e85af88da8
renzgabrinao <rgabrinao1@gmail.com>
2019-05-23 21:29:56 -0700

    add listItem

--- Assignment 2/a2.js
+++ Assignment 2/a2.js
@@ -7,3 +7,11 @@ const formatDollarAmount = (money) => {
     console.log(`$${money.toFixed(2)}`);
     
 }
+
+const addListItem = (id, text) => {
+    const node = document.createElement("li");
+    const newText = document.createTextNode(text);
+    node.appendChild(newText);
+
+    document.getElementById(id).innerHTML += node.innerHTML;
+}
