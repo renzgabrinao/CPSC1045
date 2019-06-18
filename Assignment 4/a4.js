20e82c7b4135a2bda5479d969d19ae850daa71c3
renzgabrinao <rgabrinao1@gmail.com>
2019-06-18 02:58:24 -0700

    assignment 4

--- Assignment 4/a4.js
+++ Assignment 4/a4.js
@@ -6,20 +6,53 @@ function setContent(id, text){
 
 function getAcronym(){
     let string = prompt('Enter phrase');
+
+    // .split separates the string with parameter [space] into an array
+    // "Hello World".split(' ') ==> ["Hello", "World"]
     let split = string.split(' ');
 
     let acronym = [];
     let j = 0;
+
+    // check for every index of array: if length > 0, store first char of that index in acronym
     for(let i = 0; i < split.length; i++){
         if(split[i].length > 0){
             acronym[j] = split[i].charAt(0);
             j++;
         }
     }
-
-
-    setContent("printAcronym", `${string} becomes ${(acronym.join('')).toLocaleUpperCase()}`)
     
+    // acronym.join('') combines everything in that array in one string with [nothing] as parameter
+    // ['a', 'b', 'c'].join('') ==> "abc"
+    let msg = string + " becomes " + acronym.join('').toLocaleUpperCase();
+    setContent("printAcronym", msg);    
+}
 
+function getCross(){
+    let size = 0;
+    while(!(size % 2)){
+        size = +prompt("Enter odd number");
+    }
+    let msg = "";
     
+    // round down to get middle
+    let mid = Math.floor(size / 2);
+    
+    for(let i = 0; i < size; i++){
+        for(let j = 0; j < size; j++){
+            // middle case
+            if(i === mid && j === mid){
+                msg += "+";
+            }else if(j === mid){
+                msg += "|";
+            }else if(i === mid){
+                msg += "-";
+            }else{
+                msg += " ";
+            }
+        }
+        msg += "\n";
+    }
+
+    setContent("printCross", msg);
 }
