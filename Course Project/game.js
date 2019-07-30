a71ecd7c025f7bd1683fe1c4cc6133ab0ecda85c
renzgabrinao <rgabrinao1@gmail.com>
2019-07-29 22:36:07 -0700

    update robot, and add end point

--- Course Project/game.js
+++ Course Project/game.js
@@ -7,14 +7,25 @@ const tileHeight = 100;
 // canvas dimensions 900 x 900
 let canvasHeight = 900;
 let canvasWidth = 900;
-let aiCoords = convertToCoords(0, 0);
+
+let aiCoords = {
+    x: 0,
+    y: 0
+}
+
+let directions = {
+    north: 0,
+    east: 1,
+    south: 2,
+    west: 3
+}
 
 function setup(){
     canvas = document.getElementById("myCanvas");
     ctx = canvas.getContext("2d");
+    
     setBoard();
     
-    initializeAi();
     // click on canvas to fill squares
     canvas.addEventListener("click", function (e){
         buildTower(e);
@@ -22,42 +33,122 @@ function setup(){
 }
 
 /*-------------------------------------AI--------------------------------------------*/
+function startRobot(){
+    let robot = createRobot(2, aiCoords);
+    setInterval(function (){
+        robot.move();
+    }, 250);
+}
 
-function drawAi(color){
-    gameArray[aiCoords.x][aiCoords.y] = color;
+/*  
+    Create a robot that:
+    -Has coordinates,
+    -Moves with direction
+        ~Based on the direction, check left
+        ~if theres a wall, turn right, check again
+        ~else, turn left, then move
+*/
+function createRobot(startingDirection, startingCoords){
+    return {
+        direction: startingDirection,
+        coordinates: startingCoords,
+        faceLeft: function() {
+
+            this.direction = this.direction - 1;
+            if (this.direction < 0) this.direction = 3;
+        },
+
+        faceRight: function() {           
+
+            this.direction = this.direction + 1;
+            if (this.direction > 3) this.direction = 0;
+        },
+
+        move: function (){
+
+            this.faceLeft();
+            let xCoord = this.coordinates.x;
+            let yCoord = this.coordinates.y;
+
+            // get the coordinates of the wall in front of robot
+            switch(this.direction){
+                case directions.north:
+                    yCoord -= 1;
+                    break;
+                case directions.east:
+                    xCoord += 1;
+                    break;
+                case directions.south:
+                    yCoord += 1;
+                    break;
+                case directions.west:
+                    xCoord -= 1;
+                    break;
+                default:
+                    break;
+            }
+
+            let frontCoords = { x: xCoord, y: yCoord };
+
+            // loop while there is a wall, when false then move to the free spot
+            while(isWall(frontCoords)){
+                this.faceRight();
+                switch(this.direction){
+                    case directions.north:
+                        yCoord -= 1;
+                        break;
+                    case directions.east:
+                        xCoord += 1;
+                        break;
+                    case directions.south:
+                        yCoord += 1;
+                        break;
+                    case directions.west:
+                        xCoord -= 1;
+                        break;
+                    default:
+                        break;
+                }
+                // update the coordinates of the wall
+                frontCoords = { x: xCoord, y: yCoord };
+            }
+            this.forward(frontCoords.x, frontCoords.y);
+        },
+
+        forward: function (x, y){
+            this.coordinates = {
+                x: x,
+                y: y
+            };
+            // draw robot on canvas with updated coords
+            if(this.coordinates.x === 8 && this.coordinates.y === 8){
+                alert("You lost!");
+                return false;                   
+            }else{
+                drawAi(this.coordinates, "blue");
+                drawLines();
+            }
+        }
+    }
+}
+
+// check if there is a wall or not
+function isWall(coords){
+    let isIndexInbound = function(index){
+        return index >= 0 && index < 9;
+    }
+    return !isIndexInbound(coords.x) || !isIndexInbound(coords.y) || gameArray[coords.x][coords.y] === "black";
+}
+
+function drawAi(coords, color){
+    gameArray[coords.x][coords.y] = color;
     ctx.save();
     
     ctx.fillStyle = color;
-    ctx.fillRect(aiCoords.x * tileHeight, aiCoords.y * tileHeight, tileHeight, tileHeight);
+    ctx.fillRect(coords.x * tileHeight, coords.y * tileHeight, tileHeight, tileHeight);
     ctx.restore();
-    console.log(`${color} COORDS AT ${aiCoords.x}, ${aiCoords.y}`);
 }
 
-function initializeAi(){
-    console.log(`Creating AI on (${aiCoords.x}, ${aiCoords.y})`);
-    drawAi("blue");
-    setInterval(function (){
-        moveAi();
-    }, 1000);
-}
-
-function moveAi(){
-    console.log(`Moving AI`);
-    let getRandomInt = function(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    // get open tiles
-    let openTilesArr = getOpenTiles(aiCoords);  // expecting array of coords of open tiles
-    // pick random tile
-    let newCoords = openTilesArr[getRandomInt(0, openTilesArr.length - 1)];
-    // move
-    drawAi("white");
-    aiCoords = newCoords;
-    drawAi("blue");
-    drawLines();
-}
 
 function getOpenTiles(aiCoords){
     let openTilesArr = [];
@@ -77,22 +168,21 @@ function getOpenTiles(aiCoords){
             }
         }
     }
-    console.log(openTilesArr);
     return openTilesArr;
 }
 
-
 /*-----------------------------------------------------------------------------------*/
-
 
 function buildTower(event){
     // get mouse coordinates from canvas
     let coords = convertToCoords(event.clientX, event.clientY);
-    console.log(`Clicked On ${coords.x} , ${coords.y}`);
+
     // update gameArray with coordinates from click
-    if(gameArray[coords.x][coords.y] === "white"){
+    if(gameArray[coords.x][coords.y] === "red"){
+        alert("You can't build there!");    
+    }else if(gameArray[coords.x][coords.y] === "white"){
         gameArray[coords.x][coords.y] = "black";
-    }else{
+    }else if(gameArray[coords.x][coords.y] === "black"){
         gameArray[coords.x][coords.y] = "white";
     }
 
@@ -100,23 +190,19 @@ function buildTower(event){
     for(let i = 0; i < arraySize; i++){
         for(let j = 0; j < arraySize; j++){
             ctx.save();
-
             for(let k = 0; k < tileHeight; k++){
                 if(gameArray[i][j] === "black"){
-                    ctx.fillStyle = "grey";
-                    setTimeout(function() {
-                        ctx.fillStyle = "black";
-                        ctx.fillRect((i*tileHeight), (j*tileHeight), tileHeight, k);
-                    }, 100);
-                } else if(gameArray[i][j] === "white"){
+                    ctx.fillStyle = "black";
+                    ctx.fillRect((i*tileHeight), (j*tileHeight), tileHeight, k)
+                }else if(gameArray[i][j] === "white"){
                     ctx.fillStyle = "white";
                     ctx.fillRect((i*tileHeight), (j*tileHeight), tileHeight, k);
                 }
-            }
             ctx.restore();
+            }
         }
-    }
     drawLines();       
+    }
 }
 
 // convert clientX and clientY to xy
@@ -138,7 +224,14 @@ function setBoard(){
     // set coordinates for 2d array
     for(let i = 0; i < gameArray.length; i++){
         for(let j = 0; j < gameArray.length; j++){
-            gameArray[i][j] = "white";
+            if(i === 8 && j === 8){
+                // set end goal
+                gameArray[i][j] = "red";
+                ctx.fillStyle = gameArray[i][j];
+                ctx.fillRect((i*tileHeight), (j*tileHeight), tileHeight, tileHeight);
+            }else{
+                gameArray[i][j] = "white";
+            }
         }
     }
     drawLines();
